2f7db4ba880f3d853d1dbfe677db6315b804f237
Umid <umid-berdiev82@mail.ru>
2022-01-10 18:06:04 +0500

    next commit

--- src/components/global-blocks/app-footer/index.vue
+++ src/components/global-blocks/app-footer/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <footer class="fixed bottom-0 w-full">
+  <footer class="static bottom-0 w-full py-10">
     <div class="container">
-      <h1>Footer</h1>
+      <img src="/src/assets/images/Takk footer.png" alt="takk logo" />
     </div>
   </footer>
 </template>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,51 +1,178 @@
 <template>
   <div>
     <!-- Hero rectangle -->
-    <section>
-      <div
-        class="container grid grid-cols-1 md:grid-cols-2 rounded-3xl bg-theme-2 shadow-outline"
-      >
-        <div class="my-auto">
-          <h2
-            class="text-3xl md:text-4xl text-black font-bold lg:text-5xl text-center lg:text-left mb-6"
-          >The all you need app for your coffee shop.</h2>
-          <button type="button" class="btn-primary">Sign Up</button>
+    <section id="hero-rectangle">
+      <div class="container grid grid-cols-1 md:grid-cols-2">
+        <div class="grid">
+          <div class="md:ml-16 my-auto">
+            <h2
+              class="text-3xl md:text-4xl font-bold lg:text-7xl text-center lg:text-left mb-10"
+            >
+              The all you need app for your
+              <span class="underline">coffee shop.</span>
+            </h2>
+            <button type="button" class="btn-primary">Sign Up</button>
+          </div>
         </div>
-        <div class="order-first md:order-last">
+        <div class="grid">
           <img
-            class
+            class="m-auto"
             src="/src/assets/images/Hero coffee img.png"
             alt="Hero image"
           />
         </div>
       </div>
     </section>
-    <!-- <section class="hero-rectangle">
-    <div
-      class="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-5 lg:mt-10 rounded-lg"
-    >
-      <div class="flex flex-1 flex-col lg:items-start">
-        <h2
-          class="text-3xl md:text-4xl text-black font-bold lg:text-5xl text-center lg:text-left mb-6"
-        >The all you need app for your coffee shop.</h2>
-        <div class="flex flex-wrap">
-          <button type="button" class="btn-primary">Sign Up</button>
-        </div>
-      </div>
-      <div class="flex justify-center flex-1">
-        <img src="/src/assets/images/Hero coffee img.png" alt="Hero image" />
-      </div>
-    </div>
-    </section>-->
-    <section class="preordering-rectangle rounded-lg">
+    <section id="preordering-rectangle" class="mt-10">
       <div
-        class="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-5 lg:mt-10 rounded-lg"
+        class="rectangle container grid grid-cols-1 md:grid-cols-2 bg-theme-34"
       >
-        <div class="flex flex-1 flex-col">
+        <div class="grid">
           <img
+            class="m-auto"
             src="/src/assets/images/preordering-img.png"
             alt="Preordering image"
           />
+        </div>
+        <div class="grid">
+          <div class="md:mx-5 my-auto">
+            <h2
+              class="text-3xl md:text-4xl font-bold lg:text-7xl text-center lg:text-left mb-10"
+            >Preordering</h2>
+            <p
+              class="text-2xl md:text-3xl lg:text-5xl mb-5"
+            >Accept preorders from customers for pickup.</p>
+            <p
+              class="text-xl md:text-2xl lg:text-4xl italic"
+            >*Customers not waiting in line means happier customers.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="delivery-rectangle" class="mt-10">
+      <div
+        class="rectangle container grid grid-cols-1 md:grid-cols-2 bg-theme-33"
+      >
+        <div class="grid">
+          <div class="md:ml-16 my-auto">
+            <h2
+              class="text-3xl md:text-4xl font-bold lg:text-7xl text-center lg:text-left mb-10"
+            >Delivery</h2>
+            <p
+              class="text-2xl md:text-3xl lg:text-5xl"
+            >Deliver orders over certain $ value, if the customer is located within pre-defined radius.</p>
+          </div>
+        </div>
+        <div class="grid order-first md:order-last">
+          <img
+            class="m-auto"
+            src="/src/assets/images/delivery_img.png"
+            alt="Deliver image"
+          />
+        </div>
+      </div>
+    </section>
+    <section id="loyalty-stamps-rectangle" class="mt-10">
+      <div
+        class="rectangle container grid grid-cols-1 md:grid-cols-2 bg-theme-34"
+      >
+        <div class="grid">
+          <img
+            class="m-auto"
+            src="/src/assets/images/loyalty_stamps_img.png"
+            alt="Loyalty Stamps image"
+          />
+        </div>
+        <div class="grid">
+          <div class="md:mx-5 my-auto">
+            <h2
+              class="text-3xl md:text-4xl font-bold lg:text-7xl text-center lg:text-left mb-10"
+            >Loyalty Stamps</h2>
+            <p
+              class="text-2xl md:text-3xl lg:text-5xl mb-5"
+            >No need to fuss about with printing loyalty cards.</p>
+            <p
+              class="text-xl md:text-2xl lg:text-4xl italic"
+            >*Customers earn a point for pre-defined category of purchases.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="self-checkout-rectangle" class="mt-10">
+      <div
+        class="rectangle container grid grid-cols-1 md:grid-cols-2 bg-theme-33"
+      >
+        <div class="grid">
+          <div class="md:ml-16 my-auto">
+            <h2
+              class="text-3xl md:text-4xl font-bold lg:text-7xl text-center lg:text-left mb-10"
+            >Self Checkout</h2>
+            <p
+              class="text-2xl md:text-3xl lg:text-5xl"
+            >Let the customers order and pay themselves, instead of waiting for the single cashier who is overwhelmed.</p>
+          </div>
+        </div>
+        <div class="grid order-first md:order-last">
+          <img
+            class="m-auto"
+            src="/src/assets/images/self_checkout.png"
+            alt="Deliver image"
+          />
+        </div>
+      </div>
+    </section>
+    <section id="analytics-rectangle" class="mt-10">
+      <div
+        class="rectangle container grid grid-cols-1 md:grid-cols-2 bg-theme-34"
+      >
+        <div class="grid">
+          <img
+            class="m-auto"
+            src="/src/assets/images/analytics_img.png"
+            alt="Loyalty Stamps image"
+          />
+        </div>
+        <div class="grid">
+          <div class="md:mx-5 my-auto">
+            <h2
+              class="text-3xl md:text-4xl font-bold lg:text-7xl text-center lg:text-left mb-10"
+            >
+              Analytics
+              <span
+                class="text-xl md:text-2xl lg:text-4xl italic font-medium"
+              >*coming up</span>
+            </h2>
+            <p
+              class="text-2xl md:text-3xl lg:text-5xl mb-5"
+            >Know what your customers want. Do relevant promotions to increase sales.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="fees-rectangle" class="mt-10">
+      <div class="container grid bg-theme-33">
+        <div class="grid grid-cols-12 md:my-16">
+          <div class="col-start-3 col-span-8">
+            <h2
+              class="text-3xl md:text-4xl lg:text-6xl font-extrabold text-center lg:text-left mb-10"
+            >Our fees are low, and straightforward.</h2>
+          </div>
+
+          <div class="box col-start-3 col-span-8 px-36 py-16">
+            <h1
+              class="text-4xl md:text-5xl lg:text-7xl text-center font-medium mb-10"
+            >Only Pay for Results</h1>
+            <p
+              class="text-center text-2xl md:text-3xl lg:text-5xl mb-5 font-normal"
+            >Regular use of Takk is free of charge for independent coffeeshops.</p>
+            <p
+              class="text-center text-2xl md:text-3xl lg:text-5xl mb-5 font-normal"
+            >We charge 3% of the sales, if payment is made through prepaid Takk Budget (guaranteed return customer).</p>
+            <p
+              class="text-center text-2xl md:text-3xl lg:text-5xl mb-5 font-normal"
+            >The regular card processing fees (the latest is 2.9%+30cent) still apply on each transaction and are passed on to payment processors.</p>
+            <button type="button" class="btn-primary w-full h-24">Sign Up</button>
+          </div>
         </div>
       </div>
     </section>
@@ -64,12 +191,58 @@ const str = ref("Hello Takk");
   @apply shadow-md py-3 px-6 transition duration-300 rounded-full font-bold;
 }
 .btn-primary {
-  @apply bg-theme-31 text-white border-0 shadow-lg py-4 px-12 rounded-full font-bold hover:bg-theme-2 hover:text-black text-xl;
+  @apply bg-theme-31 text-white border-0 shadow-lg py-4 px-12 rounded-full font-bold hover:bg-theme-2 hover:text-black text-4xl;
 }
 
-.hero-rectangle .container {
-  /* background-image: url("/src/assets/images/Hero Rectangle.png");
+#hero-rectangle .container {
+  background-image: url("/src/assets/images/mask_group_16.png");
   background-repeat: no-repeat;
-  background-size: cover; */
+  background-size: cover;
+  /* box-shadow: 0 0 10px 1px #e8f6f9; */
+  box-shadow: 0 0 15px #45cbeb4d;
+  border-radius: 50px;
+  min-height: 45rem;
+}
+
+#fees-rectangle .container {
+  background-image: url("/src/assets/images/mask_group_16.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  /* box-shadow: 0 0 10px 1px #e8f6f9; */
+  box-shadow: 0 0 15px #45cbeb4d;
+  border-radius: 50px;
+  min-height: 45rem;
+}
+
+#fees-rectangle .box {
+  box-shadow: 0px 10px 15px #00000026;
+  border-radius: 40px;
+}
+
+#fees-rectangle .box p {
+  color: #525252;
+  font-size: 40px;
+}
+.rectangle {
+  border: 5px solid #b2e2ed;
+  border-radius: 50px;
+  min-height: 45rem;
+}
+
+.underline {
+  text-decoration-color: #fdd5b3;
+  text-decoration-thickness: 0.4em;
+  /* text-underline-offset: -0.1em; */
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+span {
+  color: #2f2e41;
 }
 </style>
