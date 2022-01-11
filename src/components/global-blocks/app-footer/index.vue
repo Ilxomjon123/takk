49f2354648e02f8c2e0bf4182eaa28860e1eb7c7
Umid <umid-berdiev82@mail.ru>
2022-01-11 07:58:37 +0500

    next commit

--- src/components/global-blocks/app-footer/index.vue
+++ src/components/global-blocks/app-footer/index.vue
@@ -1,7 +1,25 @@
 <template>
   <footer class="static bottom-0 w-full py-10">
-    <div class="container">
-      <img src="/src/assets/images/Takk footer.png" alt="takk logo" />
+    <div class="container px-4">
+      <div class="grid grid-flow-col auto-cols-max justify-between">
+        <div class="grid grid-flow-col auto-cols-max">
+          <img src="/src/assets/images/Takk footer.png" alt="takk logo" />
+          <div class="grid grid-flow-col gap-8 ml-24">
+            <img src="/src/assets/images/38-instagram.png" alt="instagram logo" />
+            <img src="/src/assets/images/06-facebook.png" alt="facebook logo" />
+            <img src="/src/assets/images/E-mail.png" alt="mail logo" />
+          </div>
+        </div>
+        <div class="grid">
+          <ul
+            class="grid grid-flow-col items-center gap-12 text-3xl text-theme-32 font-medium"
+          >
+            <li class="cursor-pointer">Terms</li>
+            <li class="cursor-pointer">Privacy</li>
+            <li class="cursor-pointer">Copyright</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
