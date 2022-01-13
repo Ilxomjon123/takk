384c97adff929f4cdc14b50b5319d000306d5cad
Umid <umid-berdiev82@mail.ru>
2022-01-13 11:47:04 +0500

    next commit

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -190,30 +190,36 @@
     tabindex="-1"
     aria-hidden="true"
   >
-    <div class="modal-dialog modal-xl">
-      <div class="modal-content">
-        <div class="modal-body p-5 md:p-10">
-          <div class="md:flex md:flex-row md:gap-10">
-            <div class="text-center box md:basis-1/2 mb-10 md:mb-0">
-              <button class="mt-8 md:mt-16" @click="goto('/')">
+    <div class="modal-dialog modal-xl min-w-fit">
+      <div class="modal-content rounded-3xl md:rounded-large">
+        <div class="modal-body md:p-10">
+          <div class="grid md:grid-cols-2 md:gap-10 auto-cols-auto">
+            <div
+              class="grid justify-center text-center box md:p-10 mb-10 md:mb-0 h-72 md:h-auto rounded-3xl md:rounded-large"
+            >
+              <button class @click="goto('/')">
                 <img
-                  class="mx-auto w-1/2 md:w-full"
+                  class="mx-auto w-28 md:w-auto"
                   src="@/assets/images/Icon metro-shop.svg"
                   alt="For Coffee Shops Icon"
                 />
                 <h1
-                  class="text-2xl md:text-4xl font-bold mt-10"
+                  class="text-2xl md:text-4xl font-bold mt-10 whitespace-nowrap"
                 >For Coffee Shops</h1>
               </button>
             </div>
-            <div class="text-center box md:basis-1/2">
-              <button class="mt-8 md:mt-16" @click="goto('/for-customers')">
+            <div
+              class="grid justify-center text-center box md:p-10 h-72 md:h-auto rounded-3xl md:rounded-large"
+            >
+              <button class @click="goto('/for-customers')">
                 <img
-                  class="mx-auto w-1/2 md:w-full"
+                  class="mx-auto w-1/2"
                   src="@/assets/images/Group 2557.svg"
                   alt="For Costumers Icon"
                 />
-                <h1 class="text-2xl md:text-4xl font-bold mt-10">For Costumers</h1>
+                <h1
+                  class="text-2xl md:text-4xl font-bold mt-10 whitespace-nowrap"
+                >For Costumers</h1>
               </button>
             </div>
           </div>
@@ -274,7 +280,7 @@ function goto(path) {
   background-size: cover;
   /* box-shadow: 0 0 10px 1px #e8f6f9; */
   box-shadow: 0 0 15px #45cbeb4d;
-  border-radius: 50px;
+  /* border-radius: 50px; */
   /* min-height: 45rem; */
 }
 
@@ -320,9 +326,11 @@ function goto(path) {
 
 #entry-modal .box {
   box-shadow: 0 10px 15px #00000025;
-  border-radius: 50px;
+  /* border-radius: 50px; */
   background-color: #fafeff;
-  min-height: 300px;
+  /* min-height: 300px;
+  max-height: 500px;
+  height: 400px; */
 }
 
 #entry-modal .box:hover {
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,18 @@ module.exports = {
       ...theme('colors'),
       DEFAULT: primaryColors.gray['300']
     }),
+    borderRadius: {
+      none: '0',
+      sm: '0.125rem',
+      DEFAULT: '0.25rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      xl: '0.75rem',
+      '2xl': '1rem',
+      '3xl': '1.5rem',
+      full: '9999px',
+      large: '3.125rem'
+    },
     colors: {
       ...primaryColors,
       white: 'white',
