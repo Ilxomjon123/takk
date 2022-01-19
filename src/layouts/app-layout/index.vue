951972da2ed4a9f81cbdbbc3f532a35158ad2d53
Umid <umid-berdiev82@mail.ru>
2022-01-19 22:08:27 +0500

    styles fixed

--- src/layouts/app-layout/index.vue
+++ src/layouts/app-layout/index.vue
@@ -16,10 +16,14 @@ import AppTopBar from '@/components/global-blocks/app-top-bar/index.vue';
 import AppFooter from '@/components/global-blocks/app-footer/index.vue';
 import cash from 'cash-dom';
 import EntryModal from '../../components/modals/EntryModal.vue';
-import { onMounted } from 'vue';
+import { onMounted, onUnmounted } from 'vue';
 
 onMounted(() => {
   cash('html').addClass('bg-theme-2')
+})
+
+onUnmounted(() => {
+  cash('html').removeClass('bg-theme-2')
 })
 
 </script>
