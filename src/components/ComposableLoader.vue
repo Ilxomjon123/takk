6e317abfc1d5ec81b330fd8bf26f38ac7025aa3b
Ilxomjon Abdullayev <abdullayevilxomjon1@gmail.com>
2022-03-05 14:52:36 +0500

    dsa

--- src/components/ComposableLoader.vue
+++ src/components/ComposableLoader.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="isLoading"
-    class="absolute h-screen w-full z-50 flex flex-col justify-center items-center"
+    class="fixed h-screen w-full z-50 flex flex-col justify-center items-center"
     style="background-color: rgba(100, 100, 100, 0.1);"
   >
     <LoadingIcon icon="tail-spin" class="w-16 h-16" />
