134d67bb76a2da3d64f4c68cf334d049941cd2dd
Umid <umid-berdiev82@mail.ru>
2022-04-02 14:33:01 +0500

    next fixes

---
+++ src/views/dashboard/chat/SendMessageModal.vue
@@ -0,0 +1,110 @@
+<script setup>
+import { ref } from 'vue';
+import { useStore } from 'vuex';
+import { isEmpty } from 'lodash';
+import CustomerSelect from '@/components/selects/CustomerSelect.vue';
+import SimpleImageUpload from '@/components/forms/file-upload/SimpleImageUpload.vue';
+import { sendMessageToCustomers } from '@/api';
+import useWebSocket from '@/features/useWebSocket';
+
+const store = useStore()
+const selectedCustomers = ref([]);
+const editorData = ref('');
+const editorConfig = ref({
+  // extraPlugins: [uploader]
+});
+
+const { getConnection } = useWebSocket()
+const textMessage = ref(null);
+const imageMessage = ref(null);
+
+async function handleSendMessage() {
+  store.commit('setLoadingStatus', true)
+  console.log('ws connection: ', getConnection.value);
+  const customer_id = selectedCustomers.value.filter(item => item !== 'all')
+  const isAll = selectedCustomers.value.some(item => item === 'all');
+
+  const formData = new FormData();
+  !isEmpty(customer_id) && formData.append('customer_id[]', customer_id)
+  formData.append('customer_all', isAll)
+  formData.append('text', textMessage.value)
+  formData.append('files[]', imageMessage.value)
+
+  const res = await sendMessageToCustomers(formData)
+  console.log('res: ', res);
+  store.commit('setLoadingStatus', false)
+}
+
+function uploader() {
+  ClassicEditor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+    return new UploadAdapter(loader);
+  };
+}
+
+</script>
+
+<template>
+  <div
+    id="new-chatmessage-modal"
+    class="modal"
+    tabindex="-1"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <!-- BEGIN: Modal Header -->
+        <div class="modal-header">
+          <h2 class="font-medium text-base mr-auto">Create new message</h2>
+        </div>
+        <!-- END: Modal Header -->
+        <div class="modal-body">
+          <!-- multiselect select customers -->
+          <CustomerSelect v-model="selectedCustomers" />
+          <!-- message textarea -->
+          <div id="classic-editor" class="mt-5">
+            <div class>
+              <textarea v-model="textMessage" rows="5" class="form-control" />
+              <SimpleImageUpload
+                title="Add photo"
+                @update-image-file="imageMessage = $event"
+              />
+              <!-- <QuillEditor theme="snow" /> -->
+              <!-- <ckeditor
+                :editor="ClassicEditor"
+                v-model="editorData"
+                :config="editorConfig"
+              />-->
+              <!-- <component
+                :is="CKEditor.component"
+                :editor="ClassicEditor"
+                v-model="editorData"
+                :config="editorConfig"
+              />-->
+            </div>
+          </div>
+          <!-- action btns -->
+        </div>
+        <!-- BEGIN: Modal Footer -->
+        <div class="modal-footer text-right">
+          <button
+            type="button"
+            data-dismiss="modal"
+            class="btn btn-outline-secondary mr-1"
+          >Cancel</button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            @click="handleSendMessage"
+          >Create</button>
+        </div>
+        <!-- END: Modal Footer -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+textarea {
+  width: 100%;
+}
+</style>
