1ef48d60c5e33bd4824c8425ac01ceaa11e3af6e
Umid <umid-berdiev82@mail.ru>
2022-04-05 21:15:24 +0500

    next changes

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { setToken } from '../../api';
 const REQUIRED_DETAILS = 'required_details';
 
 const state = () => {
@@ -27,7 +28,7 @@ const getters = {
   getRefreshToken: state => state.refreshToken,
   getHttpHeader(state, getters) {
     return {
-      Authorization: `Basic ${getters.getToken}`
+      Authorization: `JWT ${getters.getToken}`
     };
   },
   getStep(state, getters) {
@@ -175,6 +176,7 @@ const actions = {
           // data: res.data
         };
         commit('setToken', res.data.access);
+        setToken(res.data.access);
       })
       .catch(err => {
         response = {
