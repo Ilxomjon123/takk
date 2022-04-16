94d85a0b921b5269991f529e1f67a903ca7bc5d7
Umid <umid-berdiev82@mail.ru>
2022-04-16 05:32:43 +0500

    next changes

--- src/views/dashboard/categories/Main.vue
+++ src/views/dashboard/categories/Main.vue
@@ -1,29 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+import MenuList from '@/components/lists/MenuList.vue'
+import CategoryList from '@/components/lists/CategoryList.vue'
+
+const categoryList = ref(null);
+
+function fetchData(val) {
+  categoryList.value.search();
+}
+</script>
+
 <template>
   <div>
     <!-- Menu List start -->
-    <MenuList
-      @update-id="fetchData($event)"
-      sub-item-title="Categories"
-      sub-item-value="product_category_count"
-    />
+    <MenuList @update-id="fetchData($event)" sub-item-title="Categories" sub-item-value="product_category_count" />
     <!-- Menu List end -->
 
     <CategoryList ref="categoryList" />
   </div>
 </template>
-
-<script>
-import { defineComponent } from 'vue'
-import MenuList from '../../../components/lists/MenuList.vue'
-import CategoryList from '../../../components/lists/CategoryList.vue'
-
-export default defineComponent({
-  components: { MenuList, CategoryList },
-
-  methods: {
-    fetchData(val) {
-      this.$refs.categoryList.search();
-    }
-  }
-})
-</script>
