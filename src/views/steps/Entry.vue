410099b3143893ef489b901ff62d3bc18211ceba
Ilxomjon Abdullayev <abdullayevilxomjon1@gmail.com>
2022-05-14 16:03:29 +0500

    dasd

--- src/views/steps/Entry.vue
+++ src/views/steps/Entry.vue
@@ -1,87 +1,70 @@
 
   <template>
   <!-- <DarkModeSwitcher /> -->
-  <div class="py-10">
-    <div class="container">
-      <div class="intro-y flex items-center">
-        <!-- <h2
-        class="text-xl font-medium mr-auto text-white lg:text-center"
-        >My Cabinet</h2>-->
-      </div>
-      <div class="container p-10 bg-theme-2 rounded-md mt-8 mb-24">
-        <div class="intro-y flex items-center">
-          <!-- BEGIN: Pricing Layout -->
-          <div class="intro-y box flex flex-col lg:flex-row w-full">
-            <div
-              class="intro-y border-b border-t lg:border-b-0 lg:border-t-0 flex-1 p-5 lg:border-l lg:border-r border-gray-200 dark:border-dark-5">
-              <CreditCardIcon class="block w-12 h-12 text-theme-1 dark:text-theme-10 mx-auto" />
-              <div class="text-xl font-medium text-center mt-10">Standard Plan</div>
-              <!-- <div class="text-gray-700 dark:text-gray-600 text-center mt-5">
-                1 Domain
-                <span class="mx-1">•</span> 10 Users
-                <span class="mx-1">•</span> 20 Copies
-              </div>-->
-              <div class="text-gray-600 dark:text-gray-400 px-10 text-center text-base mx-auto mt-5 min-h-[80px]">
-                No risk, 6 months free use of the Takk platform (order accepting system and mobile app). After trial
-                period, 1% of sales.
-                Plus credit card fees (2.9%+30c) passed on to payment processing companies.
-              </div>
-              <div class="mt-auto">
-                <div class="flex justify-center">
-                  <div class="relative text-5xl font-semibold mt-8 mx-auto">
-                    <span class="absolute text-2xl top-0 left-0 text-gray-600 -ml-4">$</span>
-                    0
-                  </div>
-                </div>
-                <div class="mt-auto">
-                  <a href="javascript:;" data-toggle="modal" data-target="#delete-modal-preview"
-                    class="btn btn-rounded-primary py-3 px-4 block mx-auto mt-8">PURCHASE NOW</a>
-                </div>
-              </div>
-            </div>
-            <div
-              class="intro-y border-b border-t lg:border-b-0 lg:border-t-0 flex-1 p-5 lg:border-l lg:border-r border-gray-200 dark:border-dark-5">
-              <BriefcaseIcon class="block w-12 h-12 text-theme-1 dark:text-theme-10 mx-auto" />
-              <div class="text-xl font-medium text-center mt-10">Proprietary Plan</div>
-              <!-- <div class="text-gray-700 dark:text-gray-600 text-center mt-5">
-                1 Domain
-                <span class="mx-1">•</span> 10 Users
-                <span class="mx-1">•</span> 20 Copies
-              </div>-->
-              <div class="text-gray-600 dark:text-gray-400 px-10 text-center text-base mx-auto mt-5 min-h-[80px]">Same
-                as Standard plan, plus you will have your custom mobile application with your own logo for $49 per
-                month.</div>
-              <div class="mt-auto">
-                <div class="flex justify-center">
-                  <div class="relative text-5xl font-semibold mt-8 mx-auto">
-                    <span class="absolute text-2xl top-0 left-0 text-gray-600 -ml-4">$</span>
-                    49
-                  </div>
-                </div>
-                <div class="mt-auto">
-                  <a type="button" class="btn btn-rounded-primary py-3 px-4 block mx-auto mt-8">PURCHASE NOW</a>
-                </div>
-              </div>
-            </div>
+  <div class="container mx-auto grid overflow-hidden">
+      <h2 class="py-7 text-2xl text-white">My Cabinet</h2>
+      <div class="rounded-lg flex flex-wrap -m-3 bg-white py-10 px-2 h-full">
+        <div class="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+          <div
+            class="bg-theme-1 rounded-lg shadow-lg relative overflow-hidden flex-1 flex flex-col py-8 px-3"
+          >
+            <h3 class="text-3xl text-white leading-10">
+              Run your business with us!
+            </h3>
+            <p class="py-6 text-gray-300 mb-10"></p>
+            <a href="javascript:;" data-toggle="modal" data-target="#delete-modal-preview"
+                    class="btn btn-outline-secondary bg-white w-32">Start</a>
+            <img
+              class="w-1/2 absolute -top-8 right-0"
+              src="https://www.niagarainflatables.com/wp-content/uploads/2015/03/girljump.png"
+              alt=""
+            />
           </div>
-          <!-- END: Pricing Layout -->
         </div>
-      </div>
-      <div class="hidden md:flex justify-center mx-auto w-full">
-        <router-link to="/">
-          <img class="mr-12 w-[240px] h-[80px]" src="@/assets/images/App_Store_badge_US.png" alt="App Store badge" />
-        </router-link>
-        <router-link to="/" class>
-          <img class="ml-12 w-[240px] h-[80px]" src="@/assets/images/Google_Play_Badge.png" alt="Google_Play_Badge" />
-        </router-link>
-      </div>
-      <div class="flex md:hidden justify-between">
-        <router-link to="/">
-          <img class="w-11/12" src="@/assets/images/App_Store_badge_US.png" alt="App Store badge" />
-        </router-link>
-        <router-link to="/">
-          <img class="w-11/12 ml-auto" src="@/assets/images/Google_Play_Badge.png" alt="Google_Play_Badge" />
-        </router-link>
+        <div class="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+          <div
+            class="bg-white rounded-lg shadow-lg relative overflow-hidden flex-1 flex flex-col py-8 px-3"
+          >
+            <h3 class="text-3xl text-blue-800 leading-10">
+              Download free <br />
+              Anrdoid
+            </h3>
+
+              <a class="btn btn-primary w-32 mt-auto"
+              target="blank"
+                href="https://dashboard.in"
+              >GO</a>
+            <img
+              class="w-32 absolute top-2 -right-8 origin-center -rotate-45"
+              src="https://pngimage.net/wp-content/uploads/2018/05/android-smartphone-icon-png-8.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+          <div
+            class="bg-white rounded-lg shadow-lg relative overflow-hidden flex-1 flex flex-col py-8 px-3"
+          >
+            <h3 class="text-3xl text-blue-800 leading-10">
+              Download free <br />
+              iOS
+            </h3>
+            <a class="btn btn-primary w-32 mt-auto"
+              target="blank"
+                href="https://dashboard.in"
+              >GO</a>
+            <img
+              class="w-60 absolute top-2 -right-8 origin-center -rotate-45"
+              src="../../assets/images/iphone.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="flex items-center justify-center w-full py-10">
+          <button class="btn btn-danger" @click="logout">
+            Logout
+          </button>
+        </div>
       </div>
     </div>
 
@@ -116,7 +99,6 @@
         </div>
       </div>
     </div>
-  </div>
   <ErrorNotification ref="errorNotification" />
 </template>
 
@@ -142,11 +124,11 @@ onMounted(() => {
   const step = getStep.value;
   let path;
   switch (step) {
-    // case store.state.user.STEP_COMPANY: path = '/entry/company'; break;
+    case store.state.user.STEP_COMPANY: path = '/entry/company'; break;
     case store.state.user.STEP_CAFE: path = '/entry/cafe'; break;
     case store.state.user.STEP_FINISH: path = '/entry/finish'; break;
     case store.state.user.STEP_DASHBOARD: path = '/dashboard'; break;
-    default: path = '/entry/company';
+    default: path = '/entry';
   }
   router.push(path);
 });
@@ -170,6 +152,11 @@ async function gotoSquareUpSite() {
   // const SQUARE_APP_ID = "sandbox-sq0idb-O7t0Iz9KjwN1g1l2IeSjAQ"
   // location.href = `https://connect.squareupsandbox.com/login?redirect=/oauth2/authorize?client_id=${SQUARE_APP_ID}&scope=CUSTOMERS_WRITE+CUSTOMERS_READ`
   // location.href = `https://connect.squareupsandbox.com/oauth2/authorize?client_id=${SQUARE_APP_ID}&scope=CUSTOMERS_WRITE+CUSTOMERS_READ`
+}
+
+function logout(){
+  store.dispatch('logout');
+  window.location.replace('/')
 }
 
 </script>
