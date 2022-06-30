891e7bd80b2e4150c8fe3708efdb3cf75f2a2225
Umid <umid-berdiev82@mail.ru>
2022-06-30 16:45:06 +0500

    next commit

--- src/layouts/app-layout/index.vue
+++ src/layouts/app-layout/index.vue
@@ -1,3 +1,23 @@
+<script setup>
+import AppTopBar from '@/components/global-blocks/app-top-bar/index.vue';
+import AppFooter from '@/components/global-blocks/app-footer/index.vue';
+import cash from 'cash-dom';
+import EntryModal from '@/components/modals/EntryModal.vue';
+import MobileOverlayMenu from '@/components/overlay-menus/mobile-menu/index.vue';
+import { onMounted, onUnmounted } from 'vue';
+import ContactUsModal from '@/components/modals/ContactUsModal.vue';
+
+onMounted(() => {
+  cash('html').addClass('bg-theme-2')
+  cash('html body').css('padding', 0);
+})
+
+onUnmounted(() => {
+  cash('html').removeClass('bg-theme-2')
+})
+
+</script>
+
 <template>
   <div class="content">
     <AppTopBar />
@@ -12,26 +32,6 @@
   <MobileOverlayMenu />
   <ContactUsModal />
 </template>
-
-<script setup>
-import AppTopBar from '@/components/global-blocks/app-top-bar/index.vue';
-import AppFooter from '@/components/global-blocks/app-footer/index.vue';
-import cash from 'cash-dom';
-import EntryModal from '@/components/modals/EntryModal.vue';
-import MobileOverlayMenu from '@/components/overlay-menus/mobile-menu/index.vue';
-import { onMounted, onUnmounted } from 'vue';
-import ContactUsModal from '@/components/modals/ContactUsModal.vue';
-
-onMounted(() => {
-  cash('html').addClass('bg-theme-2')
-  cash('body').css('padding', 0);
-})
-
-onUnmounted(() => {
-  cash('html').removeClass('bg-theme-2')
-})
-
-</script>
 
 <style scoped>
 .content {
