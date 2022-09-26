0c6407fd4770555724ab83e505d452887825aecc
Umid <umid-berdiev82@mail.ru>
2022-09-26 21:22:37 +0500

    next changes

--- src/views/dashboard/cafes/CafeItemCard.vue
+++ src/views/dashboard/cafes/CafeItemCard.vue
@@ -28,7 +28,7 @@ const cafeMainPhoto = computed(() =>
         />
       </figure>
       <div
-        class="bg-dark-1/50 text-theme-5 rounded-xl rounded-t-none absolute bottom-0 w-full p-3"
+        class="bg-dark-1/50 rounded-xl rounded-t-none absolute bottom-0 w-full p-3"
       >
         <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">
           {{ cafe.name }}
